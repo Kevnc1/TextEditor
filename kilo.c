277a8213a6127b12d8594a0e09633fc9575215e0
Kevnc1 <kevincalle07@gmail.com>
2024-08-05 20:56:36 -0400

    Added feature to confirm quit if modified and not saved

--- kilo.c
+++ kilo.c
@@ -19,6 +19,7 @@
 
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8 
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -511,6 +512,7 @@ void editorMoveCursor(int key) {
     }
 }
 void editorProcessKeypress(void) {
+    static int quit_times  = KILO_QUIT_TIMES;
     int c  = editorReadKey();
     
     switch (c) {
@@ -518,6 +520,12 @@ void editorProcessKeypress(void) {
             /* TODO */
             break;
         case CTRL_KEY('q'):
+            if (E.dirty && quit_times > 0) {
+                editorSetStatusMessage("Warning. File has unsaved changes. "
+                    "Press Ctrl-Q %d more times to quit.", quit_times);
+                quit_times--;
+                return;
+            }
             write(STDOUT_FILENO, "\x1b[2J", 4); 
             write(STDOUT_FILENO, "\x1b[H", 3); 
             exit(0);
@@ -567,6 +575,8 @@ void editorProcessKeypress(void) {
             editorInsertChar(c);
             break;
     }
+
+    quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
