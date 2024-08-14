9d15c988ec5531baee06f7b753aad53f3682dda7
Kevnc1 <kevincalle07@gmail.com>
2024-08-14 12:02:15 -0400

    Implemented basic search feature

--- kilo.c
+++ kilo.c
@@ -198,6 +198,21 @@ int editorRowCxToRx(erow *row, int cx) {
     return rx;
 
 }
+
+int editorRowRxToCx(erow *row, int rx) {
+    int cur_rx = 0;
+    int cx;
+    for (cx = 0; cx < row->size; cx++) {
+        if (row->chars[cx] == '\t') {
+            cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+        }
+        cur_rx++;
+        
+        if (cur_rx > rx) return cx;
+    }
+    return cx;
+}
+
 void editorUpdateRow(erow *row) {
     // count tabs
     int tabs = 0;
@@ -401,6 +416,27 @@ void editorSave(void) {
     free(buf);
     editorSetStatusMessage("Can't save. I/O error: %s", strerror(errno));
 }
+
+/*** find ***/
+
+void editorFind(void) {
+    char *query = editorPrompt("Search: %s (ESC to cancel)");
+    if (query == NULL) return;
+    
+    int i;
+    for (i = 0; i < E.numrows; i++) {
+        erow *row = &E.row[i];
+        char *match = strstr(row->render, query);
+        if (match) {
+            E.cy = i;
+            E.cx = editorRowRxToCx(row, match - row->render);
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+    free(query);
+}
+
 /*** append buffer ***/
 
 struct abuf {
@@ -644,6 +680,9 @@ void editorProcessKeypress(void) {
         case CTRL_KEY('s'):
             editorSave();
             break;
+        case CTRL_KEY('f'):
+            editorFind();
+            break; 
         case HOME_KEY:
             E.cx = 0;
             break;
@@ -717,7 +756,7 @@ int main(int argc, char *argv[]) {
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP: Ctrl-S = save |  Ctrl-Q = quit");
+    editorSetStatusMessage("HELP: Ctrl-S = save |  Ctrl-Q = quit | CTRL-F = find");
 
     while (1) {
         editorRefreshScreen();
