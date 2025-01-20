e58ac29a72ab969f8ce5aea539a53e14f13cae84
Kevnc1 <kevincalle07@gmail.com>
2025-01-20 17:39:57 -0500

    Enabled syntax highlighting for C keywords

--- kilo.c
+++ kilo.c
@@ -2,6 +2,8 @@
 #define _BSD_SOURCE
 #define _GNU_SOURCE
 
+/*** includes ***/
+
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -21,7 +23,7 @@
 #define KILO_TAB_STOP 8 
 #define KILO_QUIT_TIMES 3
 
-#define CTRL_KEY(k) ((k) & 0x1f)
+#define CTRL_KEY(k) ((k) & 0x1f) // get ascii ctrl char code
 
 enum editorKey {
     BACKSPACE = 127,
@@ -39,6 +41,8 @@ enum editorKey {
 enum editorHighlight {
     HL_NORMAL = 0,
     HL_COMMENT,
+    HL_KEYWORD1,
+    HL_KEYWORD2,
     HL_STRING,
     HL_NUMBER,
     HL_MATCH
@@ -51,7 +55,8 @@ enum editorHighlight {
 
 struct editorSyntax {
     char *filetype; // name of filetype
-    char **filematch; // strings containing pattern to match filetype
+    char **filematch; // strings containing pattern to match filetype name
+    char **keywords; 
     char *singleline_comment_start // single line comment pattern for filetype
     int flags;
 };
@@ -86,11 +91,16 @@ struct editorConfig E;
 /*** filetypes ***/
 
 char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
+char *C_HL_keywords[] = {"switch", "if", "while", "for", "break", "continue", "return", "else",
+                         "struct", "union", "typedef", "static", "enum","class", "case",
+                         "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+                         "void|", NULL"}
 
 struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extensions,
+        C_HL_keywords,
         "//",
         HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
     },
@@ -231,6 +241,8 @@ void editorUpdateSyntax(erow *row){
     memset(row->hl, HL_NORMAL, row->rsize);
 
     if (E.syntax == NULL) return;
+
+    char **keywords = E.syntax->keywords;
     
     char *scs = E.syntax->singleline_comment_start;
     int scs_len = scs ? strlen(scs) : 0;
@@ -282,6 +294,28 @@ void editorUpdateSyntax(erow *row){
                 continue;
             }
         }
+        
+        if (prev_sep) {
+            int j;
+            for (j = 0; keywords[j]; j++) {
+                int klen = strlen(keywords[j]);
+                int kw2 = keywords[j][klen - 1] == '|'; 
+                if (kw2) klen--; // indicates common type
+            
+                if (!strncmp(&row->render[i], keywords[j], klen) &&
+                    is_separator(row->render[i+klen])) {
+                        memset(&row->hl[i], kw2 ? HL_KEYWORD2 : HL_KEYWORD1, klen);
+                    i += klen;
+                    break;
+                }
+            }
+
+            if (keywords[j] != NULL) {
+                prev_sep = 0;
+                continue;
+            }
+        }
+
         prev_sep = is_separator(c);
         i++;
     }
@@ -290,6 +324,8 @@ void editorUpdateSyntax(erow *row){
 int editorSyntaxToColor(int hl) {
     switch(hl) {
         case HL_COMMENT: return 36;
+        case HL_KEYWORD1: return 33;
+        case HL_KEYWORD2: return 32;
         case HL_STRING: return 35;
         case HL_NUMBER: return 31;
         case HL_MATCH: return 34;
