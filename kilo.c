a0d461f4d43f4e7bb517fd97f13bda43acefa12e
Kevnc1 <kevincalle07@gmail.com>
2025-06-10 18:46:25 -0400

    Displayed nonprintable characters

--- kilo.c
+++ kilo.c
@@ -757,7 +757,18 @@ void editorDrawRows(struct abuf *ab) {
             int current_color  = -1;
 
             for (int j = 0; j < len; j++) {
-                if (hl[j] == HL_NORMAL) {
+                if (iscntrl(c[j])) {
+                    char sym = (c[j] <= 26) ? '@' + c[j] : '?';
+                    abAppend(ab, "\x1b[7m", 4); // turn on invert color
+                    abAppend(ab, &sym, 1);
+                    abAppend(ab, "\x1b[m", 3); // turn off invert color
+                    if (current_color != -1) { // 
+                        char buf[16];
+                        int clen = snprintf(buf, sizeof(buf), "\x1b[%dm", current_color);
+                        abAppend(ab, buf, clen);
+                    }
+                }
+                else if (hl[j] == HL_NORMAL) {
                     if (current_color != -1) {
                         abAppend(ab, "\x1b[39m", 5);
                         current_color = -1;
